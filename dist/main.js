1ebb16dbc3eafcf4d1845caa6b05fbb91f3debae
Sandro <sandro@paetzold.co.uk>
2019-06-02 10:09:58 +0100

    Change JS target for bundling

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,33 @@
+class Harvester {
+    static run(creep) {
+        const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+        }
+        else {
+            creep.drop(RESOURCE_ENERGY);
+        }
+    }
+}
+class SpawnManager {
+    run() {
+    }
+    checkUnits(type) {
+        // Unit ratio
+        // let unitTypes: number = 6;
+        // let harvesterRatio: number = 1 / unitTypes;
+        // let carrierRatio: number = 1 / unitTypes;
+        // let upgraderRatio: number = 1 / unitTypes;
+        // let builderRatio: number = 1 / unitTypes * 2;
+        // let repairerRatio: number = 1 / unitTypes;
+        // let harvesterCurrent = _.countBy(Game.creeps, (c) => c.memory.role == 'harvester');
+    }
+    spawn() {
+        Game.spawns['London'].spawnCreep([MOVE, WORK, WORK], 'H-' + Game.time, {
+            memory: { role: 'harvester', isBusy: false }
+        });
+    }
+}
 /// <reference path="spawnManager.ts" />
 /// <reference path="harvester.ts" />
 let unitManager = new SpawnManager();
