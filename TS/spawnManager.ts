fec84508f726fa2be0314887a2592cb9c8aa655a
Sandro <sandro@paetzold.co.uk>
2019-06-02 21:40:05 +0100

    Add unit spawning

--- TS/spawnManager.ts
+++ TS/spawnManager.ts
@@ -1,20 +1,66 @@
 class SpawnManager {
 
-    private checkUnits(type: string) {
+    private checkUnits(type: string): boolean {
         // Unit ratio
-        // let unitTypes: number = 6;
-        // let harvesterRatio: number = 1 / unitTypes;
-        // let carrierRatio: number = 1 / unitTypes;
-        // let upgraderRatio: number = 1 / unitTypes;
-        // let builderRatio: number = 1 / unitTypes * 2;
-        // let repairerRatio: number = 1 / unitTypes;
+        let unitTypes: number = 6;
+        let harvesterRatio: number = 1 / unitTypes;
+        let carrierRatio: number = 1 / unitTypes;
+        let upgraderRatio: number = 1 / unitTypes;
+        let builderRatio: number = 1 / unitTypes * 2;
+        let repairerRatio: number = 1 / unitTypes;
 
-        // let harvesterCurrent = _.countBy(Game.creeps, (c) => c.memory.role == 'harvester');
+        // Total units
+        let totalScreeps: number = _.size(Game.creeps);
+
+        // Current units
+        let harvesterCurrent: number = _.filter(Game.creeps, (c) => c.memory.role == 'harvester').length;
+        let carrierCurrent: number = _.filter(Game.creeps, (c) => c.memory.role == 'carrier').length;
+        let upgraderCurrent: number = _.filter(Game.creeps, (c) => c.memory.role == 'upgrader').length;
+        let builderCurrent: number = _.filter(Game.creeps, (c) => c.memory.role == 'builder').length;
+        let repairerCurrent: number = _.filter(Game.creeps, (c) => c.memory.role == 'repairer').length;
+
+        if (type === 'harvester') {
+            if (harvesterCurrent / totalScreeps < harvesterRatio) { return true; }
+            return false;
+        } else if (type === 'carrier') {
+            if (carrierCurrent / totalScreeps < carrierRatio) { return true; }
+            return false;
+        } else if (type === 'upgrader') {
+            if (upgraderCurrent / totalScreeps < upgraderRatio) { return true; }
+            return false;
+        } else if (type === 'builder') {
+            if (builderCurrent / totalScreeps < builderRatio) { return true; }
+            return false;
+        } else if (type === 'repairer') {
+            if (repairerCurrent / totalScreeps < repairerRatio) { return true; }
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public spawn() {
-        Game.spawns['London'].spawnCreep([MOVE, WORK, WORK], 'H-' + Game.time, {
-            memory: { role: 'harvester', isBusy: false }
-        });
+        if (this.checkUnits('harvester')) {
+            Game.spawns['London'].spawnCreep([MOVE, WORK, WORK], 'H-' + Game.time, {
+                memory: { role: 'harvester', isBusy: false }
+            });
+        } else if (this.checkUnits('carrier')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, MOVE, MOVE, CARRY, CARRY, CARRY], 'C-' + Game.time, {
+                memory: { role: 'carrier', isBusy: false }
+            })
+        } else if (this.checkUnits('upgrader')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'U-' + Game.time, {
+                memory: { role: 'upgrader', isBusy: false }
+            })
+        } else if (this.checkUnits('builder')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'B-' + Game.time, {
+                memory: { role: 'builder', isBusy: false }
+            })
+        } else if (this.checkUnits('repairer')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'R-' + Game.time, {
+                memory: { role: 'repairer', isBusy: false }
+            })
+        }
+
     }
 }
--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 class Harvester {
     static run(creep) {
-        const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
         if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
@@ -11,11 +11,78 @@ class Harvester {
 }
 class SpawnManager {
     checkUnits(type) {
+        let unitTypes = 6;
+        let harvesterRatio = 1 / unitTypes;
+        let carrierRatio = 1 / unitTypes;
+        let upgraderRatio = 1 / unitTypes;
+        let builderRatio = 1 / unitTypes * 2;
+        let repairerRatio = 1 / unitTypes;
+        let totalScreeps = _.size(Game.creeps);
+        let harvesterCurrent = _.filter(Game.creeps, (c) => c.memory.role == 'harvester').length;
+        let carrierCurrent = _.filter(Game.creeps, (c) => c.memory.role == 'carrier').length;
+        let upgraderCurrent = _.filter(Game.creeps, (c) => c.memory.role == 'upgrader').length;
+        let builderCurrent = _.filter(Game.creeps, (c) => c.memory.role == 'builder').length;
+        let repairerCurrent = _.filter(Game.creeps, (c) => c.memory.role == 'repairer').length;
+        if (type === 'harvester') {
+            if (harvesterCurrent / totalScreeps < harvesterRatio) {
+                return true;
+            }
+            return false;
+        }
+        else if (type === 'carrier') {
+            if (carrierCurrent / totalScreeps < carrierRatio) {
+                return true;
+            }
+            return false;
+        }
+        else if (type === 'upgrader') {
+            if (upgraderCurrent / totalScreeps < upgraderRatio) {
+                return true;
+            }
+            return false;
+        }
+        else if (type === 'builder') {
+            if (builderCurrent / totalScreeps < builderRatio) {
+                return true;
+            }
+            return false;
+        }
+        else if (type === 'repairer') {
+            if (repairerCurrent / totalScreeps < repairerRatio) {
+                return true;
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
     }
     spawn() {
-        Game.spawns['London'].spawnCreep([MOVE, WORK, WORK], 'H-' + Game.time, {
-            memory: { role: 'harvester', isBusy: false }
-        });
+        if (this.checkUnits('harvester')) {
+            Game.spawns['London'].spawnCreep([MOVE, WORK, WORK], 'H-' + Game.time, {
+                memory: { role: 'harvester', isBusy: false }
+            });
+        }
+        else if (this.checkUnits('carrier')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, MOVE, MOVE, CARRY, CARRY, CARRY], 'C-' + Game.time, {
+                memory: { role: 'carrier', isBusy: false }
+            });
+        }
+        else if (this.checkUnits('upgrader')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'U-' + Game.time, {
+                memory: { role: 'upgrader', isBusy: false }
+            });
+        }
+        else if (this.checkUnits('builder')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'B-' + Game.time, {
+                memory: { role: 'builder', isBusy: false }
+            });
+        }
+        else if (this.checkUnits('repairer')) {
+            Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, WORK, CARRY], 'R-' + Game.time, {
+                memory: { role: 'repairer', isBusy: false }
+            });
+        }
     }
 }
 let unitManager = new SpawnManager();
