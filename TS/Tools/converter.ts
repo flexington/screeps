2f006ef81157f8da054875197811aa8fe6416661
Sandro Paetzold <sandro@paetzold.co.uk>
2019-06-09 15:34:36 +0100

    Update for IPosition

--- TS/Tools/converter.ts
+++ TS/Tools/converter.ts
@@ -3,7 +3,7 @@ class Converter {
      * Converts a given IRoomPosition to an instance of RoomPosition
      * @param position IRoomPosition
      */
-    public static toRoomPosition(position: IRoomPosition) {
+    public static toRoomPosition(position: IPosition) {
         return new RoomPosition(position.x, position.y, position.room);
     }
 }
