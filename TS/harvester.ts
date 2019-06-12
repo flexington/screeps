2f4eb8fe5150a0cb8dc03a8e484c59f3e27812e8
Sandro Paetzold <sandro@paetzold.co.uk>
2019-06-12 22:15:12 +0100

    remove old code

--- TS/harvester.ts
+++ TS/harvester.ts
@@ -2,20 +2,12 @@ class Harvester {
     private static config: IHarvester
 
     public static run(creep: Creep) {
-        // Init creep
-        this.config = creep.memory.config;
-        if (this.config === undefined) { this.init(); }
+        // if (!this.config.atSource && creep.pos != Converter.toRoomPosition(this.config.position)) creep.moveTo(Converter.toRoomPosition(this.config.position));
+        // else this.config.atSource = true;
 
-        if (!this.config.atSource && creep.pos != Converter.toRoomPosition(this.config.position)) creep.moveTo(Converter.toRoomPosition(this.config.position));
-        else this.config.atSource = true;
-
-        if (this.config.atSource && creep.harvest(Game.getObjectById(this.config.source.id)) == OK) creep.drop(RESOURCE_ENERGY);
+        // if (this.config.atSource && creep.harvest(Game.getObjectById(this.config.source.id)) == OK) creep.drop(RESOURCE_ENERGY);
     }
 
     private static init() {
-        let sources = GameManager.config.sources;
-        for (let i = 0, source: ISource; source = sources[i]; i++) {
-            
-        }
     }
 }
